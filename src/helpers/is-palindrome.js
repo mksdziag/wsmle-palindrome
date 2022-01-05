c91297dbf48c542feaf63b8fa6103a8b8ad1e522
Maks <mksdziag@gmail.com>
2022-01-05 09:09:43 +0100

    feat: :sparkles: Add options to checkPalindrome helper

--- src/helpers/is-palindrome.js
+++ src/helpers/is-palindrome.js
@@ -1,5 +1,11 @@
-export function checkPalindrome(str) {
+export function checkPalindrome(
+  str = "",
+  { trim, caseSensitive } = { trim: true, caseSensitive: false }
+) {
   if (str.length === 0) return false;
+
+  str = trim ? str.trim() : str;
+  str = caseSensitive ? str : str.toLowerCase();
 
   return str == str.split("").reverse().join("");
 }
