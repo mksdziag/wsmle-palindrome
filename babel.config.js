89a1dde85db69169b0d6f73d958afbf1d1ff70a3
Maks <mksdziag@gmail.com>
2022-01-05 09:12:00 +0100

    build: :construction_worker: Update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,19 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        useBuiltIns: "usage",
-        corejs: 3,
-      },
+module.exports = (api) => {
+  return {
+    plugins: ["@babel/plugin-transform-runtime"],
+
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "entry",
+          corejs: 3,
+          // caller.target will be the same as the target option from webpack
+          targets: api.caller((caller) => caller && caller.target === "node")
+            ? { node: "current" }
+            : { chrome: "58", ie: "10" },
+        },
+      ],
     ],
-  ],
+  };
 };
